ef62600743db17a5d24f349cc3ec26f4e9d232d4
Tyler Pachal <tylerpachalandfriends@gmail.com>
2026-02-27 09:24:42 -0400

    Append to stream

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -8,4 +8,25 @@ defmodule MyApp do
       :ok = MyApp.CommandedApplication.dispatch(%MyApp.Command.Create{name: "Tyler"})
     end
   end
+
+  def append_many_directly_to_store(amount \\ 10) do
+    event = %MyApp.Event.Created{name: "Tyler"}
+
+    event_data = %Commanded.EventStore.EventData{
+      causation_id: nil,
+      correlation_id: nil,
+      event_type: Commanded.EventStore.TypeProvider.to_string(event),
+      data: event,
+      metadata: %{}
+    }
+
+    events = Enum.map(1..amount, fn _ -> event_data end)
+
+    Commanded.EventStore.append_to_stream(
+      MyApp.CommandedApplication,
+      "Tyler",
+      :any_version,
+      events
+    )
+  end
 end
